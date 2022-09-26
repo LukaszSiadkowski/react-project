15e050dcd068360d450c50bfb98342bd725174d1
Lukasz Siadkowski <l.siadkowski@yahoo.com>
2022-09-26 21:28:38 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,16 +8,18 @@ app.use(cors());
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.get('/articles', (req, res) => {
-    newsapi
-    .getMainArticles(req.query)
-    .then((response) => {res.json(response.body);})
-});
+
 app.get('/psy-news', (req, res) => {
     newsapi
     .getPsyNews(req.query)
     .then((response) => {res.json(response.body);})
 });
+app.get('/articles', (req, res) => {
+    newsapi
+    .getMainArticles(req.query)
+    .then((response) => {res.json(response.body);})
+});
+
 
 
 
